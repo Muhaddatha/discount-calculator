66324e609536d562bd7771a2b0864e46c15dd275
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2021-01-03 16:15:24 -0500

    created the summaries component

--- src/components/Summaries.js
+++ src/components/Summaries.js
@@ -1,9 +1,15 @@
 import React from "react";
+import DiscountSummary from "./components/DiscountSummary"
 
-const Summaries = () => {
+const Summaries = ({summaries}) => {
     return(
         <div id="summary-container">
-            <ul className="summary-list"></ul>
+            <ul className="summary-list">
+                {/* mapping a summary to a list element */}
+                {summaries.map(summary => (
+                    <DiscountSummary price={summary.price} discount={summary.discount} finalPrice={summary.finalPrice}/>
+                ))}
+            </ul>
         </div>
     );
 }
