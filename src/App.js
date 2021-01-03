2321ddb2d3caeacd4830596e8613ee531b37e63c
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2021-01-03 13:55:43 -0500

    added summaries component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Form from "./components/Form"
+import Form from "./components/Form";
+import Summaries from "./components/Summaries";
 
 function App() {
   return (
     <div className="App">
       <h1>Hi</h1>
       <Form />
+      <Summaries />
     </div>
   );
 }
---
+++ src/components/Summaries.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Summaries = () => {
+    return(
+        <div id="summary-container">
+            <ul className="summary-list"></ul>
+        </div>
+    );
+}
+
+export default Summaries;
