fdbb845c1d96035e6aef1ed94751624e19420daf
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2021-01-03 19:23:44 -0500

    comments to app.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import Summaries from "./components/Summaries";
 
 function App() {
 
+  //variables used to store the state of the elements
   const [priceText, setPriceText] = useState("");
   const [discountText, setDiscountText] = useState(""); 
+  //array that holds all the calculations
   const [summaries, setSummaries] = useState([]);
 
   return (
-    <div className="App">
-      <h1>Hi</h1> 
+    <div className="App"> 
+      {/* form components manipulates the data that will be appended to the summaries
+      container. */}
       <Form summaries={summaries} setSummaries={setSummaries} priceText={priceText} setPriceText={setPriceText} discountText={discountText} setDiscountText={setDiscountText}/>
+      
+      {/* All calculation summaries are appended here. */}
       <Summaries summaries={summaries} setSummaries={setSummaries} />
     </div>
   );
