74b887d30234ccb9e0745696628a58eb5d08fbcb
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2021-01-03 19:12:22 -0500

    created summary card addition

--- src/components/Summaries.js
+++ src/components/Summaries.js
@@ -1,13 +1,13 @@
 import React from "react";
 import DiscountSummary from "./DiscountSummary"
 
-const Summaries = ({summaries}) => {
+const Summaries = ({summaries, setSummaries}) => {
     return(
         <div id="summary-container">
             <ul className="summary-list">
                 {/* mapping a summary to a list element */}
                 {summaries.map(summary => (
-                    <DiscountSummary key={summary.id} price={summary.price} discount={summary.discount} finalPrice={summary.finalPrice} />
+                    <DiscountSummary key={summary.id} summaries={summaries} setSummaries={setSummaries} summary={summary} />
                 ))}
             </ul>
         </div>
